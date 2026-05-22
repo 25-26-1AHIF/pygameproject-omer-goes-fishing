a611e495081a6a0644346a4249c8a65d29ad676c
Ömer Atum <oemer.atum@studnet.htl-rankweil.at>
2026-05-22 11:55:37 +0200

    Screens

--- Ömer_goes_fishing/src/main.py
+++ Ömer_goes_fishing/src/main.py
@@ -2,8 +2,32 @@ import pygame
 from game_variables.game_variables import GameVariables as gv
 from game_variables.game_variables import GameScreens
 
-def main_screen(screen: pygame.Surface, clock: pygame.time.Clock) -> str:
+def play_screen(screen: pygame.Surface, clock: pygame.time.Clock):
+    pygame.display.set_caption("Play Screen")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return GameScreens.EXIT
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    return GameScreens.MAIN
+
+        screen.fill("black")
+        pygame.display.flip()
+        clock.tick(gv.FPS)
+
+def main_screen(screen: pygame.Surface, clock: pygame.time.Clock) -> GameScreens:
     pygame.display.set_caption("Main Screen")
+
+    titel_text = gv.FONT_BIG.render("Ömer goes fishing", True, "white")
+    starten_text = gv.FONT_MIDDLE.render("Start", True, "white")
+    controls_text = gv.FONT_MIDDLE.render("Controls", True, "white")
+    exit_text = gv.FONT_MIDDLE.render("Exit", True, "white")
+
+    title_text_rect = titel_text.get_rect(center = (gv.SCREEN_WIDTH // 1.2, 100))
+    starten_text_rect = starten_text.get_rect(center = (gv.SCREEN_WIDTH // 4, 250))
+    controls_text_rect = controls_text.get_rect(center = (gv.SCREEN_WIDTH // 4, 350))
+    exit_text_rect = exit_text.get_rect(center = (gv.SCREEN_WIDTH // 4, 450))
 
     while True:
         for event in pygame.event.get():
@@ -12,8 +36,23 @@ def main_screen(screen: pygame.Surface, clock: pygame.time.Clock) -> str:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     return GameScreens.EXIT
-
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if starten_text_rect.collidepoint(event.pos):
+                    print("Start!")
+                    return GameScreens.PLAY
+                if controls_text_rect.collidepoint(event.pos):
+                    print("Controls!")
+                    #Todo Controls Screen
+                if exit_text_rect.collidepoint(event.pos):
+                    print("Exit!")
+                    return GameScreens.EXIT
         screen.fill("black")
+        screen.blit(titel_text, title_text_rect)
+        screen.blit(starten_text, starten_text_rect)
+        screen.blit(controls_text, controls_text_rect)
+        screen.blit(exit_text, exit_text_rect)
+
+
         pygame.display.flip()
         clock.tick(gv.FPS)
 
