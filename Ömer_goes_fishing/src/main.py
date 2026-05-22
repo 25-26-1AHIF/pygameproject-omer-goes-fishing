4b65211b90f12335e719f21f47d1d1480680338e
Ömer Atum <oemer.atum@studnet.htl-rankweil.at>
2026-05-22 11:05:32 +0200

    Main main

--- Ömer_goes_fishing/src/main.py
+++ Ömer_goes_fishing/src/main.py
@@ -1,1 +1,20 @@
 import pygame
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption("Main")
+    clock = pygame.time.Clock()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+
+        screen.fill("black")
+        pygame.display.flip()
+        clock.tick(60)
+
+if __name__ == '__main__':
+    main()
