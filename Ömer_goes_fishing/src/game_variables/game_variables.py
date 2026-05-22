076edfb0f7358599b3791ab750cee41454230ef1
Ömer Atum <oemer.atum@studnet.htl-rankweil.at>
2026-05-22 11:32:07 +0200

    verbesserung main / game variables

--- Ömer_goes_fishing/src/game_variables/game_variables.py
+++ Ömer_goes_fishing/src/game_variables/game_variables.py
@@ -1,21 +1,21 @@
 import pygame
 
 class GameVariables:
-  FPS = 60
+    FPS = 60
 
-  FONT_BIG: pygame.font.Font = None
-  FONT_MIDDLE: pygame.font.Font = None
-  FONT_SMALL: pygame.font.Font = None
+    FONT_BIG: pygame.font.Font = None
+    FONT_MIDDLE: pygame.font.Font = None
+    FONT_SMALL: pygame.font.Font = None
 
-  @staticmethod
-  def init():
-    pygame.init()
-    GameVariables.FONT_BIG = pygame.font.SysFont("arial", 48, bold=True)
-    GameVariables.FONT_MIDDLE = pygame.font.SysFont("arial", 30, bold=False)
-    GameVariables.FONT_SMALL = pygame.font.SysFont("arial", 14, bold=False)
+    @staticmethod
+    def init():
+        pygame.init()
+        GameVariables.FONT_BIG = pygame.font.SysFont("arial", 48, bold=True)
+        GameVariables.FONT_MIDDLE = pygame.font.SysFont("arial", 30, bold=False)
+        GameVariables.FONT_SMALL = pygame.font.SysFont("arial", 14, bold=False)
 
 class GameScreens:
-  MAIN = "mainscreen"
-  PLAY = "play"
-  EXIT = "exit"
-  actual = MAIN
+    MAIN = "mainscreen"
+    PLAY = "play"
+    EXIT = "exit"
+    actual = MAIN
--- Ömer_goes_fishing/src/main.py
+++ Ömer_goes_fishing/src/main.py
@@ -1,20 +1,34 @@
 import pygame
+from game_variables.game_variables import GameVariables as gv
+from game_variables.game_variables import GameScreens
 
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((800, 600))
-    pygame.display.set_caption("Main")
-    clock = pygame.time.Clock()
+def main_screen(screen: pygame.Surface, clock: pygame.time.Clock) -> str:
+    pygame.display.set_caption("Main Screen")
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                return
+                return GameScreens.EXIT
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    return GameScreens.EXIT
 
         screen.fill("black")
         pygame.display.flip()
-        clock.tick(60)
+        clock.tick(gv.FPS)
+
+def main():
+    gv.init()
+    screen = pygame.display.set_mode((800, 600))
+    clock = pygame.time.Clock()
+
+    while True:
+        if GameScreens.actual == GameScreens.MAIN:
+            GameScreens.actual = main_screen(screen, clock)
+        elif GameScreens.actual == GameScreens.EXIT:
+            break
+
+    pygame.quit()
 
 if __name__ == '__main__':
     main()
